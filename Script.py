34cf0faf2b813640991783e8f32745411eba3744
VAD3R <maitreyam29@gmail.com>
2017-04-29 20:10:31 +0530

    Script.py
    Robust Script for ctf`s and cracking

--- Script.py
+++ Script.py
@@ -1,6 +1,6 @@
 '''
 for refernce for ddiferent ctf`s and online cracks...(contact me ----VAD3R)
-''
+'''
 import os
 import urllib.request
 import re
@@ -14,20 +14,20 @@ import numpy as np
 
 
 def post(passw):
-    entry={'metu':passw}                            # entering qrcode with  at metu place
+    entry={'qrcode':qrcode}                            # entering qrcode with  at qrcode place
     senddata = urllib.parse.urlencode(entry)
     return bytes(senddata, 'utf-8')
 
 
 
-url = 'http://challenge01.root-me.org/programmation/ch7/'
+url = ' '
 
 start_time = time.time()
 cj = http.cookiejar.CookieJar()                     # using httpcookiejar for automatic handling of cookies
 opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
 urllib.request.install_opener(opener)
 
-req = urllib.request.Request(url)                    # GET request to ch7 page
+req = urllib.request.Request(url)                    # GET request to page
 resp = urllib.request.urlopen(req)
 respData = resp.read()
     
@@ -40,27 +40,27 @@ for eachP in paragraphs:
     r = eachP
 print (r)
                                                    # writing our image to a file
-with open('quick.png', 'wb') as f:
+with open('img.png', 'wb') as f:
     data = f.write(text)
 
                                                      
-WHITE =(255,255,255)                                 # drawing finders square on corners
-W=                                                  # your width height 
+WHITE =(255,255,255)                                 # fixing imafe to ber read (on corners)
+W=                                                   # your width height 
 BLACK = (0,0,0)
-image = Image.open("quick.png")
+image = Image.open("img.png")
 draw = ImageDraw.Draw(image)
 for (x,y) in [(18, 18), (18,218), (218,18)]:
     draw.rectangle((x,y, , ),BLACK, BLACK)
     draw.rectangle((x+W,y+W, , ),WHITE, WHITE)
     draw.rectangle((x+2*W,y+2*W, , ),BLACK, BLACK)
 
-image.save('result2.png')
+image.save('result.png')
 
 '''
 
  transformation:                                # (optional)
  morhpological , translational, affine           # open saved image to transform the image 
- see:codes.py for more ;-)
+ see:codes.py for more.
  
 '''
 
@@ -78,7 +78,7 @@ print('QR codes: %s' % codes)
 
 ans = post(qrcode)
 print(ans)
-answer = urllib.request.urlopen(url, ans)          # POST req to ch8 page with qrcode
+answer = urllib.request.urlopen(url, ans)          # POST req to page with qrcode
 raw = answer.read()
 raw = raw.decode('utf-8')
 print(raw)
